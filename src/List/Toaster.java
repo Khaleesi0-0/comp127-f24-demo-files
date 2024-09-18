f63824a9e16250f554ee15951e2152ea0adeb302
Khaleesi0-0 <166758118+Khaleesi0-0@users.noreply.github.com>
2024-09-18 11:03:55 -0500

    9/15

---
+++ src/List/Toaster.java
@@ -0,0 +1,24 @@
+package List;
+
+public class Toaster {
+    //state
+    private int intensity;
+    private int numslot;
+    private int isEmpty;
+
+
+
+    //behavior 
+    public Toaster(int numslot){
+        this.numslot = numslot;
+        this.isEmpty = 1;
+        this.intensity = true;
+        this.bread = null;
+
+
+    }
+    public boolean load(String bread){
+        if(this.isEmpty)
+    }
+
+}
