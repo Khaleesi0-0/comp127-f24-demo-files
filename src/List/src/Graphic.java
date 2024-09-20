a6626bbe4d95c03f5fcb35850c017d83752d0b1e
Khaleesi0-0 <166758118+Khaleesi0-0@users.noreply.github.com>
2024-09-20 08:40:41 -0500

    9/18

---
+++ src/List/src/Graphic.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+
+import edu.macalester.graphics.CanvaWindow;
+import edu.macalester.graphics.Rectangle;
+
+public class Graphic {
+    public static void main(String[] args) {
+        CanvaWindow canvas = new CanvaWindow("Drawing", 500, 500);
+        
+    }
+}
